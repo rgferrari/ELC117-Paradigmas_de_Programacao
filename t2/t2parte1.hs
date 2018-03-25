601e198b7b50f22143f07ea0671e114a2e5f1bfb
rgferrari <rgferrari@inf.ufsm.br>
2018-03-25 13:45:10 -0300

    exercicio 3 feito

--- t2/t2parte1.hs
+++ t2/t2parte1.hs
@@ -7,5 +7,10 @@ geraTabela x = (x, x ^ 2 ) : geraTabela ( x - 1 )
 checkChar :: Char -> String -> Bool
 checkChar c [] = False;
 checkChar c (x:str)
-	| c == x = True
-	| otherwise = checkChar c str
+        | c == x = True
+        | otherwise = checkChar c str
+
+--exercicio 3
+coordenadas :: [(Float,Float)] -> [(Float,Float)]
+coordenadas [] = [];
+coordenadas ((x,x1):xs) = (x+2, x1+2) : coordenadas xs
