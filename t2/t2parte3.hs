6d1f2f8278f5522146532aa923909d9850d0e046
rgferrari <rgferrari@inf.ufsm.br>
2018-03-31 15:27:05 -0300

    parte 3 feita

---
+++ t2/t2parte3.hs
@@ -0,0 +1,23 @@
+toInt :: String -> [Int]
+toInt [] = []
+toInt (x:xs) = ( read [x] :: Int ) : ( toInt xs )
+
+isEanOk :: String -> Bool
+isEanOkAux3 [] = 0
+isEanOkAux3 xs = ( ( head xs ) * 3 ) + ( isEanOkAux1 ( tail xs ) )
+isEanOkAux1 [] = 0
+isEanOkAux1 xs = ( ( head xs ) * 1 ) + ( isEanOkAux3 ( tail xs ) )
+isEanOk xs
+    | ( length xs ) /= 13 = undefined
+    | otherwise = ( ( last (toInt xs) ) + ( isEanOkAux1 (init (toInt xs) ) ) ) `mod` 10 == 0
+
+ 
+
+
+
+
+
+
+
+--"6291041500213" 
+--"5901234123457"
